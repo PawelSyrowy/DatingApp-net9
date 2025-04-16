d4ea219b6c0a62477c56d533ee02fd909281648e
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-04-16 23:21:18 +0200

    End of section 5 FIX

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, HomeComponent]
+})
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAnimations()
-  ]
-};
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+
+}
