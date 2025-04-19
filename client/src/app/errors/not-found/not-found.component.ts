8ee050a1cba62ae39a051572ad404edc77166636
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-04-19 13:27:56 +0200

    End of section 7

--- client/src/app/errors/not-found/not-found.component.ts
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-not-found',
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
-})
-export class NotFoundComponent {
-
-}
+ import { RouterLink } from '@angular/router';
+ 
+ @Component({
+   selector: 'app-not-found',
+   standalone: true,
+   imports: [RouterLink],
+   templateUrl: './not-found.component.html',
+   styleUrl: './not-found.component.css'
+ })
+ export class NotFoundComponent {
+ 
+ }
