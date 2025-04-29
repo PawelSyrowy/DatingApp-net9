40cab340bb33ad503b8f861e57941c12ccf561f8
MENELOZO\pawco <pawelsyrowy2@gmail.com>
2025-04-29 17:37:08 +0200

    End of section 18

--- client/src/app/modals/confirm-dialog/confirm-dialog.component.ts
+++ client/src/app/modals/confirm-dialog/confirm-dialog.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-confirm-dialog',
+  standalone: true,
   imports: [],
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.css'
+  styleUrl: './confirm-dialog.component.css',
 })
 export class ConfirmDialogComponent {
+  bsModalRef = inject(BsModalRef);
+  title = '';
+  message = '';
+  btnOkText = '';
+  btnCancelText = '';
+  result = false;
 
+  confirm() {
+    this.result = true;
+    this.bsModalRef.hide();
+  }
+
+  decline() {
+    this.bsModalRef.hide();
+  }
 }
